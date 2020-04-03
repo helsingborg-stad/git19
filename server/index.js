704cc41e590de851ddd760cf5f1baa18c3ad61ab
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-04-03 23:44:08 +0200

    Add express & serve client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable global-require */
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    require('dotenv').config();
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 }
 
 const express = require('express');
@@ -10,6 +10,8 @@ const path = require('path');
 const http = require('http');
 const https = require('https');
 const netjet = require('netjet');
+
+// const routes = require('./components/routes');
 
 const app = express();
 const API_BASE = '/api/v1';
@@ -20,12 +22,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Static assets
 // app.use(netjet(true, { cache: { maxAge: 300000 } }));
-app.use(express.static(`${process.cwd()}/build`, { maxage: '2h' }));
+app.use(express.static(`${process.cwd()}/client/build`, { maxage: '2h' }));
 
 // Static
 app.get('/', (request, response) => {
-  //   response.set('Cache-Control', 'public, max-age=31557600');
-  response.sendFile(path.join(`${process.cwd()}/public/index.html`));
+    response.set('Cache-Control', 'public, max-age=31557600');
+    response.sendFile(path.join(`${process.cwd()}/build/index.html`));
 });
 
 // Endpoints
@@ -33,16 +35,16 @@ app.get('/', (request, response) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server listening on port ', PORT);
+    console.log('Server listening on port ', PORT);
 });
 
 // Prevent Heroku from sleeping
 if (typeof process.env.HEROKU_URL !== 'undefined') {
-  setInterval(function() {
-    if (process.env.HEROKU_URL.startsWith('https')) {
-      https.get(process.env.HEROKU_URL);
-      return;
-    }
-    http.get(process.env.HEROKU_URL);
-  }, 300000); // every 5 minutes (300000)
+    setInterval(function() {
+        if (process.env.HEROKU_URL.startsWith('https')) {
+            https.get(process.env.HEROKU_URL);
+            return;
+        }
+        http.get(process.env.HEROKU_URL);
+    }, 300000); // every 5 minutes (300000)
 }
